f4792669df59fe7f48ce3c710f2908c6baa36710
emmanuel <110382530+emmanuel-dev000@users.noreply.github.com>
2022-12-29 18:14:09 +0800

    Updated: product-card with product's metadata

--- resources/views/sarisari/app/user/product/product-card.blade.php
+++ resources/views/sarisari/app/user/product/product-card.blade.php
@@ -1,10 +1,14 @@
 <div class="col-md-4">
     <div class="product-item">
-      <a href="#"><img src="assets/images/product_04.jpg" alt=""></a>
+      {{-- <a href="#"><img src="assets/images/product_04.jpg" alt=""></a> --}}
+      <a href="#"><img src="{{ asset('images/product-images') }}" alt=""></a>
       <div class="down-content">
-        <a href="#"><h4>Tittle goes here</h4></a>
-        <h6>$15.25</h6>
-        <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p>
+        {{-- <a href="#"><h4>Tittle goes here</h4></a> --}}
+        <a href="#"><h4>{{ $product->name }}</h4></a>
+        {{-- <h6>$15.25</h6> --}}
+        <h6>${{ $product->price }}</h6>
+        {{-- <p>Lorem ipsume dolor sit amet, adipisicing elite. Itaque, corporis nulla aspernatur.</p> --}}
+        <p>{{ $product->description }}</p>
         <ul class="stars">
           <li><i class="fa fa-star"></i></li>
           <li><i class="fa fa-star"></i></li>
