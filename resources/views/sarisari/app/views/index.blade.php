e239a1c917d236aaaa114b1655c347b5f6244b34
emmanuel <110382530+emmanuel-dev000@users.noreply.github.com>
2022-12-30 10:44:25 +0800

    Updated: index page

--- resources/views/sarisari/app/views/index.blade.php
+++ resources/views/sarisari/app/views/index.blade.php
@@ -1,1 +1,48 @@
 @extends('sarisari.app.layout.layout')
+
+@section('banner')
+<div class="banner header-text">
+    <div class="owl-banner owl-carousel">
+      <div class="banner-item-01">
+        <div class="text-content">
+          <h4>Best Offer</h4>
+          <h2>New Arrivals On Sale</h2>
+        </div>
+      </div>
+      <div class="banner-item-02">
+        <div class="text-content">
+          <h4>Flash Deals</h4>
+          <h2>Get your best products</h2>
+        </div>
+      </div>
+      <div class="banner-item-03">
+        <div class="text-content">
+          <h4>Last Minute</h4>
+          <h2>Grab last minute deals</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+@endsection
+
+@section('sub-content')
+<div class="call-to-action">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+            <div class="inner-content">
+            <div class="row">
+                <div class="col-md-8">
+                <h4>Creative &amp; Unique <em>Sixteen</em> Products</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque corporis amet elite author nulla.</p>
+                </div>
+                <div class="col-md-4">
+                <a href="#" class="filled-button">Purchase Now</a>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</div>
+@endsection
