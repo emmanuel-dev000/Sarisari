7ec5efe20d847baf3ccfb24fc3b97c09aa9d067b
emmanuel <110382530+emmanuel-dev000@users.noreply.github.com>
2022-12-30 12:16:33 +0800

    Modifed: separated css and scripts from layout blade

---
+++ resources/views/sarisari/app/component/css.blade.php
@@ -0,0 +1,14 @@
+<!-- Bootstrap core CSS -->
+<link href="{{ asset('layouts/sixteen-clothing/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<!--
+
+TemplateMo 546 Sixteen Clothing
+
+https://templatemo.com/tm-546-sixteen-clothing
+
+-->
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="{{ asset('layouts/sixteen-clothing/assets/css/fontawesome.css') }}">
+<link rel="stylesheet" href="{{ asset('layouts/sixteen-clothing/assets/css/templatemo-sixteen.css') }}">
+<link rel="stylesheet" href="{{ asset('layouts/sixteen-clothing/assets/css/owl.css') }}">
--- resources/views/sarisari/app/layout/layout.blade.php
+++ resources/views/sarisari/app/layout/layout.blade.php
@@ -14,20 +14,7 @@
 
     <title>@yield('subtitle') - {{ config('app.name') }}</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="{{ asset('layouts/sixteen-clothing/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
-<!--
-
-TemplateMo 546 Sixteen Clothing
-
-https://templatemo.com/tm-546-sixteen-clothing
-
--->
-
-    <!-- Additional CSS Files -->
-    <link rel="stylesheet" href="{{ asset('layouts/sixteen-clothing/assets/css/fontawesome.css') }}">
-    <link rel="stylesheet" href="{{ asset('layouts/sixteen-clothing/assets/css/templatemo-sixteen.css') }}">
-    <link rel="stylesheet" href="{{ asset('layouts/sixteen-clothing/assets/css/owl.css') }}">
+    @include('sarisari.app.component.css')
 
   </head>
 
@@ -43,31 +30,7 @@ https://templatemo.com/tm-546-sixteen-clothing
 
     @include('sarisari.app.component.footer')
 
-
-    <!-- Bootstrap core JavaScript -->
-    <script src="{{ asset('layouts/sixteen-clothing/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('layouts/sixteen-clothing/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-
-
-    <!-- Additional Scripts -->
-    <script src="{{ asset('layouts/sixteen-clothing/assets/js/custom.js') }}"></script>
-    <script src="{{ asset('layouts/sixteen-clothing/assets/js/owl.js') }}"></script>
-    <script src="{{ asset('layouts/sixteen-clothing/assets/js/slick.js') }}"></script>
-    <script src="{{ asset('layouts/sixteen-clothing/assets/js/isotope.js') }}"></script>
-    <script src="{{ asset('layouts/sixteen-clothing/assets/js/accordions.js') }}"></script>
-
-
-    <script language = "text/Javascript"> 
-      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
-      function clearField(t){                   //declaring the array outside of the
-      if(! cleared[t.id]){                      // function makes it static and global
-          cleared[t.id] = 1;  // you could use true and false, but that's more typing
-          t.value='';         // with more chance of typos
-          t.style.color='#fff';
-          }
-      }
-    </script>
-
+    @include('sarisari.app.component.scripts')
 
   </body>
 
