b554d6aa85a2535bf4219050e7bf72a7f520aebf
emmanuel-dev000 <emmanuelpanganwork@gmail.com>
2023-01-10 14:33:24 +0800

    Deleted: removed comments in account blade file

--- resources/views/sarisari/app/component/account.blade.php
+++ resources/views/sarisari/app/component/account.blade.php
@@ -1,21 +1,3 @@
-{{-- <li class="nav-item dropdown">
-    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        {{ Auth::user()->name }}
-    </a>
-    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-      <a class="dropdown-item" href="{{ url('/sarisari/'.Auth::user()->name.'/profile') }}">Profile</a>
-      <div class="dropdown-divider"></div>
-        <a class="dropdown-item" href="{{ route('logout') }}"
-            onclick="event.preventDefault(); 
-            document.getElementById('logout-form').submit();">
-            Logout
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-    </div>
-</li> --}}
-
 @if (Route::has('login'))
     @auth
         <li class="nav-item">
